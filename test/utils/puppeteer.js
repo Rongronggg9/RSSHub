5048d65889147e28f1f87243830751dc309ae1a1
Rongrong <i@rong.moe>
2022-05-30 11:21:35 +0800

    fix(test): ReferenceError in puppeteer test
    also replace google.com with cloudflare.com to make it pass in PRC
    
    Signed-off-by: Rongrong <i@rong.moe>

--- test/utils/puppeteer.js
+++ test/utils/puppeteer.js
@@ -2,16 +2,29 @@ let puppeteer;
 const wait = require('../../lib/utils/wait');
 const cheerio = require('cheerio');
 
+let browser_slot = null;
+
 afterEach(() => {
+    if (browser_slot) {
+        browser_slot.close(); // double insurance if test fails
+        browser_slot = null;
+    }
     jest.resetModules();
+});
+
+afterAll(() => {
+    // https://stackoverflow.com/questions/50793885/referenceerror-you-are-trying-to-import-a-file-after-the-jest-environment-has
+    jest.useFakeTimers();
 });
 
 describe('puppeteer', () => {
     it('puppeteer run', async () => {
         puppeteer = require('../../lib/utils/puppeteer');
         const browser = await puppeteer();
+        browser_slot = browser;
+        const startTime = Date.now();
         const page = await browser.newPage();
-        await page.goto('https://www.google.com', {
+        await page.goto('https://www.cloudflare.com/', {
             waitUntil: 'domcontentloaded',
         });
 
@@ -20,12 +33,16 @@ describe('puppeteer', () => {
         expect(html.length).toBeGreaterThan(0);
 
         expect((await browser.process()).signalCode).toBe(null);
-        await wait(31 * 1000);
+        const sleepTime = 31 * 1000 - (Date.now() - startTime);
+        if (sleepTime > 0) {
+            await wait(sleepTime);
+        }
         expect((await browser.process()).signalCode).toBe('SIGKILL');
     }, 40000);
     it('puppeteer without stealth', async () => {
         puppeteer = require('../../lib/utils/puppeteer');
         const browser = await puppeteer({ stealth: false });
+        browser_slot = browser;
         const page = await browser.newPage();
         await page.goto('https://bot.sannysoft.com', {
             waitUntil: 'networkidle0',
@@ -43,6 +60,7 @@ describe('puppeteer', () => {
     it('puppeteer with stealth', async () => {
         puppeteer = require('../../lib/utils/puppeteer');
         const browser = await puppeteer({ stealth: true });
+        browser_slot = browser;
         const page = await browser.newPage();
         await page.goto('https://bot.sannysoft.com', {
             waitUntil: 'networkidle0',
